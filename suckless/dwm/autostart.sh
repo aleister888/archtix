82704f1a53ccd10fe67fc8cedd9223160734f218
aleister888 <pacoe1000@protonmail.com>
2025-06-02 20:09:26 +0200

    No iniciar aplicaciones con dwm

--- suckless/dwm/autostart.sh
+++ suckless/dwm/autostart.sh
@@ -117,10 +117,3 @@ fi
 
 # Limpiar directorio $HOME
 cleaner
-
-# Iniciar aplicaciones más usadas
-pgrep tauonmb || tauon &
-pgrep "$BROWSER" || eval "$BROWSER" &
-pgrep keepassxc || keepassxc &
-pgrep thunderbird || thunderbird &
-pgrep -f telegram-desktop || telegram-deskop &
